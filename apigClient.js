2b355135a46ae494e05e049172643663fe85b18c
EC2 Default User <ec2-user@ip-172-31-85-84.ec2.internal>
2020-05-09 17:05:06 +0000

    fixed api call

--- apigClient.js
+++ apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://tqmfhxj6fk.execute-api.us-east-1.amazonaws.com/PAQ';
+    var invokeUrl = 'https://snrjrf6xt7.execute-api.us-east-1.amazonaws.com/PAQ';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
